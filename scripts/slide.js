0c87331308affd27a499b53173fa4b7051493088
casandrabenpor <casandra211@gmail.com>
2023-05-16 11:36:39 +0200

    room-detail-desktop

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,6 +1,7 @@
 let init = false;
 let swiper = undefined;
 let swiperFeatures = undefined;
+let swiperGallery = undefined;
 
 function swiperCard(){
   if(window.innerWidth < 1000 && !init){
@@ -29,36 +30,27 @@ function swiperCard(){
         clickable: true,
       },
     });
-    
+    swiperFeatures = new Swiper(".swiper-galleryHotel",{
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
     init = true;
   }
   else if (init){
     swiper.destroy();
     swiperFeatures.destroy();
+    swiperGallery.destroy();
     init = false;
   }
 }
 swiperCard();
 window.addEventListener("resize" ,swiperCard);
 
-// const swiper = new Swiper(".swiper-gallery", {
-//     direction: "horizontal",
-//     slidesPerView: "1",
-//     // spaceBetween: 30,
-//     centeredSlides: true,
-//     // autoplay: {
-//     //   delay: 3000,
-//     //   disableOnInteraction: false,
-//     // },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//       // type: "bullets"
-//     },
-//     breakpoints:{
-//       1000 : {
-//         slidesPerView : 6,
-//         // spaceBetween: 30
-//       }
-//     }
-//   });
