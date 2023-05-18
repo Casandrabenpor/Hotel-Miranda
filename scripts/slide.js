06ea9b7954b3028230b932e2c47b7ceaabcb6b2c
casandrabenpor <casandra211@gmail.com>
2023-05-18 23:37:32 +0200

    slice

--- scripts/slide.js
+++ scripts/slide.js
@@ -90,14 +90,30 @@
 // swiperCard();
 // window.addEventListener("resize" ,swiperCard);
 
-// var swiper = new Swiper(".mySwiper", {
-//   loop: true,
-//   pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//   },
+let swiperPagination = new Swiper(".swiper-custom-pagination", {
+  loop: true,
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+});
 
-// });
+let swiperButton = new Swiper(".swiper-custom-button", {
+    loop: true,
+    slidesPerView: 2,
+    spacesBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+        1000: {
+          slidesPerView: 2,
+          spaceBetween: 100,
+        },
+      },
+  });
+
 // var swiper = new Swiper(".swiper-gallery", {
 //   loop: false,
 //   navigation: {
