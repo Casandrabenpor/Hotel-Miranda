5aa0aafb2bcec42af6ec9c6dbf9aa363536026f4
casandrabenpor <casandra211@gmail.com>
2023-05-18 22:06:15 +0200

    slice

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,92 +1,107 @@
-let init = false;
-let swiper = undefined;
-let swiperFeatures = undefined;
-let swiperGallery = undefined;
-let swiperSection = undefined;
-let swiperHotel = undefined;
+// let init = false;
+// let swiper = undefined;
+// let swiperFeatures = undefined;
+// let swiperGallery = new Swiper(".swiper-galleryHotel",{
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   slidesPerView: 1,
+//   // autoplay: {
+//   //   delay: 3000,
+//   //   disableOnInteraction: false,
+//   // },
+//   // pagination: {
+//   //   el: ".swiper-pagination",
+//   //   clickable: true,
+//   // },
+// });
+// let swiperSection = undefined;
+// let swiperHotel = undefined;
 
-function swiperCard(){
-  if(window.innerWidth < 1000 && !init){
-    
-    swiper = new Swiper(".swiper-gallery", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-    swiperFeatures = new Swiper(".swiper-features",{
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-    swiperGallery = new Swiper(".swiper-galleryHotel",{
-      spaceBetween: 30,
-      centeredSlides: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      },
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      // pagination: {
-      //   el: ".swiper-pagination",
-      //   clickable: true,
-      // },
-    });
-    swiperSection = new Swiper(".swiper-section",{
-      spaceBetween: 30,
-      centeredSlides: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      },
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-    swiperHotel = new Swiper(".swiper-hotel",{
-      spaceBetween: 60,
-      centeredSlides: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-    init = true;
-  }
-  else if (init){
-    swiper.destroy();
-    swiperFeatures.destroy();
-    swiperGallery.destroy();
-    swiperSection.destroy();
-    swiperHotel.destroy();
-    init = false;
-  }
-}
-swiperCard();
-window.addEventListener("resize" ,swiperCard);
+// function swiperCard(){
+//   if(window.innerWidth < 1000 && !init){
 
+//     swiper = new Swiper(".swiper-gallery", {
+//       spaceBetween: 30,
+//       centeredSlides: true,
+//       autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//       },
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     });
+//     swiperFeatures = new Swiper(".swiper-features",{
+//       spaceBetween: 30,
+//       centeredSlides: true,
+//       autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//       },
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     });
+
+//     swiperSection = new Swiper(".swiper-section",{
+//       spaceBetween: 30,
+//       centeredSlides: true,
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev"
+//       },
+//       autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//       },
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     });
+//     swiperHotel = new Swiper(".swiper-hotel",{
+//       spaceBetween: 60,
+//       centeredSlides: true,
+//       autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//       },
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     });
+//     init = true;
+//   }
+//   else if (init){
+//     // swiper.destroy();
+//     // swiperFeatures.destroy();
+//     // swiperSection.destroy();
+//     // swiperHotel.destroy();
+//     init = false;
+//   }
+// }
+// swiperCard();
+// window.addEventListener("resize" ,swiperCard);
+
+// var swiper = new Swiper(".mySwiper", {
+//   loop: true,
+//   pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//   },
+
+// });
+// var swiper = new Swiper(".swiper-gallery", {
+//   loop: false,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
